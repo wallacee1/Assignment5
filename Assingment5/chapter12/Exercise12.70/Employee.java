ae3657807e2fa6f716bf35533334aede47b42b3e
Eric Wallace <90478940+wallacee1@users.noreply.github.com>
2023-04-09 14:52:11 -0400

    Last of chapter12

---
+++ Assingment5/chapter12/Exercise12.70/Employee.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Employee here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface Employee
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Employee
+     */
+    public Employee()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
